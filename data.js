935e8eb257924c1ca91797603e31497583e6e5b8
DicedRPG <dicedrpg@gmail.com>
2025-04-01 08:24:49 -0500

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,7 @@ const LEVELS = [
 const QUEST_DATA = [
   {
     "id": 1,
-    "questName": "Kitchen Safety & Cleaning",
+    "questName": "Kitchen Safety & testing",
     "rank": "Home Cook",
     "type": "Training",
     "primaryFocus": "Management",
